76ad77fecc9df5f7f5141ed7c13fd4e279bdf2da
Amarelshrief <amarelshrief511@gmail.com>
2025-04-03 08:58:53 +0200

    Create "Footer.tsx" component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,110 @@
+import { Bird, Facebook, Instagram, X, Youtube } from "lucide-react";
+import { Button } from "../ui/button";
+import { Link } from "react-router-dom";
+
+function Footer() {
+  return (
+    <footer>
+      <div className="bg-[var(--primary)] pb-8">
+        <div className="main-container grid grid-cols-4 gap-18 p-2">
+          <div>
+            <div className="flex items-center gap-1 cursor-pointer pb-2">
+              <Bird />
+              <h1 className="text-2xl font-bold">BlackBird</h1>
+            </div>
+            <p className="text-sm leading-6">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Auctor
+              libero id et, in gravida. Sit diam duis mauris nulla cursus. Erat
+              et lectus vel ut sollicitudin elit at amet.
+            </p>
+            <div className="cursor-pointer flex items-center gap-4 mt-3">
+              <Button
+                variant="ghost"
+                className="bg-[var(--secondary)] hover:bg-[var(--secondary)]/40 hover:text-[#bb6b59] duration-150"
+              >
+                <Link to={"/"}> Shop Now </Link>
+              </Button>
+              <Button
+                variant="ghost"
+                className="bg-[var(--secondary)] hover:bg-[var(--secondary)]/40 hover:text-[#bb6b59] duration-150"
+              >
+                <Link to={"/"}> Send Message </Link>
+              </Button>
+            </div>
+          </div>
+          <div className="flex flex-col gap-4">
+            <h2 className="text-lg font-bold">About Us</h2>
+            <ul className="flex flex-col gap-2 w-3/4">
+              <Link to={"/"}>
+                <li className="cursor-pointer hover:text-[#bb6b59] text-sm duration-150">
+                  Careers
+                </li>
+              </Link>
+              <Link to={"/"}>
+                <li className="cursor-pointer hover:text-[#bb6b59] text-sm duration-150">
+                  Our Stores
+                </li>
+              </Link>
+              <Link to={"/"}>
+                <li className="cursor-pointer hover:text-[#bb6b59] text-sm duration-150">
+                  Our Cares
+                </li>
+              </Link>
+              <Link to={"/"}>
+                <li className="cursor-pointer hover:text-[#bb6b59] text-sm duration-150">
+                  Terms & Conditions
+                </li>
+              </Link>
+              <Link to={"/"}>
+                <li className="cursor-pointer hover:text-[#bb6b59] text-sm duration-150">
+                  Privacy Policy
+                </li>
+              </Link>
+            </ul>
+          </div>
+          <div className="flex flex-col gap-4">
+            <h2 className="text-lg font-bold">Customer Care</h2>
+            <ul className="flex flex-col gap-2 w-3/4">
+              <Link to={"/"}>
+                <li className="cursor-pointer hover:text-[#bb6b59] text-sm duration-150">
+                  Help Center
+                </li>
+              </Link>
+              <Link to={"/"}>
+                <li className="cursor-pointer hover:text-[#bb6b59] text-sm duration-150">
+                  Track Orders
+                </li>
+              </Link>
+              <Link to={"/"}>
+                <li className="cursor-pointer hover:text-[#bb6b59] text-sm duration-150">
+                  Corporate & Bulk Purchasing
+                </li>
+              </Link>
+              <Link to={"/"}>
+                <li className="cursor-pointer hover:text-[#bb6b59] text-sm duration-150">
+                  Returns & Refunds
+                </li>
+              </Link>
+            </ul>
+          </div>
+          <div className="flex flex-col gap-4">
+            <h2 className="text-lg font-bold">Contact Us</h2>
+            <p className="text-sm">
+              70 Washington Square South, New York, NY 10012, United States
+            </p>
+            <p className="text-sm">Email: uilib.help@gmail.com</p>
+            <p className="text-sm">Phone: +1 (212) 555-1234</p>
+            <div className="flex items-center gap-4">
+              <X className="cursor-pointer hover:text-[#bb6b59] duration-150" />
+              <Instagram className="cursor-pointer hover:text-[#bb6b59] duration-150" />
+              <Facebook className="cursor-pointer hover:text-[#bb6b59] duration-150" />
+              <Youtube className="cursor-pointer hover:text-[#bb6b59] duration-150" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
