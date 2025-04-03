93638e84a32577231e96c0bd11a39e946a1a5926
Amarelshrief <amarelshrief511@gmail.com>
2025-04-03 09:00:26 +0200

    Create "Home.tsx" page

---
+++ src/Pages/Home.tsx
@@ -0,0 +1,11 @@
+import SecondHeader from "@/components/Header/SecondHeader";
+
+function Home() {
+  return (
+    <div className="mb-4">
+      <SecondHeader />
+    </div>
+  );
+}
+
+export default Home;
