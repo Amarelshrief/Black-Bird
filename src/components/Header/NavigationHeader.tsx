c71178d7de4a2114e627b5a3980a1ec64b3da50c
Amarelshrief <amarelshrief511@gmail.com>
2025-04-04 22:33:23 +0200

    Add "NavigationHeader.tsx" component

---
+++ src/components/Header/NavigationHeader.tsx
@@ -0,0 +1,199 @@
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu";
+import { Cable, Music, Shirt, Watch } from "lucide-react";
+
+function NavigationHeader() {
+  return (
+    <header>
+      <div className="main-container flex items-center justify-between m-8">
+        <div>
+          <NavigationMenu>
+            <NavigationMenuList>
+              <NavigationMenuItem>
+                <NavigationMenuTrigger className="text-lg font-bold hover:bg-transparent cursor-pointer">
+                  Categories
+                </NavigationMenuTrigger>
+                <NavigationMenuContent>
+                  <div className="flex items-center gap-2 text-md p-2 cursor-pointer font-bold hover:bg-[var(--input)]  duration-300 rounded-sm h-12">
+                    <Shirt className="text-sm" />
+                    <NavigationMenuLink className="hover:bg-transparent">
+                      Fashion
+                    </NavigationMenuLink>
+                  </div>
+                  <div className="flex items-center gap-2 text-md p-2 cursor-pointer font-bold hover:bg-[var(--input)]  duration-300 rounded-sm h-12">
+                    <Cable className="text-sm" />
+                    <NavigationMenuLink className="hover:bg-transparent">
+                      Electronics
+                    </NavigationMenuLink>
+                  </div>
+                  <div className="flex items-center gap-2 text-md p-2 cursor-pointer font-bold hover:bg-[var(--input)]  duration-300 rounded-sm h-12">
+                    <Watch className="text-sm" />
+                    <NavigationMenuLink className="hover:bg-transparent">
+                      Watches
+                    </NavigationMenuLink>
+                  </div>
+                  <div className="flex items-center gap-2 text-md p-2 cursor-pointer font-bold hover:bg-[var(--input)]  duration-300 rounded-sm h-12">
+                    <Music className="text-sm" />
+                    <NavigationMenuLink className="hover:bg-transparent">
+                      Music
+                    </NavigationMenuLink>
+                  </div>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
+        </div>
+        <div>
+          <ul className="flex items-center gap-6">
+            <li>
+              <NavigationMenu>
+                <NavigationMenuList>
+                  <NavigationMenuItem>
+                    <NavigationMenuTrigger className="cursor-pointer">
+                      Home
+                    </NavigationMenuTrigger>
+                    <NavigationMenuContent>
+                      <div className="flex items-center gap-2 text-md p-2 cursor-pointer font-bold hover:bg-[var(--input)]  duration-300 rounded-sm h-12">
+                        <NavigationMenuLink className="hover:bg-transparent">
+                          Market
+                        </NavigationMenuLink>
+                      </div>
+                      <div className="flex items-center gap-2 text-md p-2 cursor-pointer font-bold hover:bg-[var(--input)]  duration-300 rounded-sm h-12">
+                        <NavigationMenuLink className="hover:bg-transparent">
+                          Gadget
+                        </NavigationMenuLink>
+                      </div>
+                      <div className="flex items-center gap-2 text-md p-2 cursor-pointer font-bold hover:bg-[var(--input)]  duration-300 rounded-sm h-12">
+                        <NavigationMenuLink className="hover:bg-transparent">
+                          Fashion
+                        </NavigationMenuLink>
+                      </div>
+                      <div className="flex items-center gap-2 text-md p-2 cursor-pointer font-bold hover:bg-[var(--input)]  duration-300 rounded-sm h-12">
+                        <NavigationMenuLink className="hover:bg-transparent">
+                          Electronics
+                        </NavigationMenuLink>
+                      </div>
+                    </NavigationMenuContent>
+                  </NavigationMenuItem>
+                </NavigationMenuList>
+              </NavigationMenu>
+            </li>
+            <li>
+              <NavigationMenu>
+                <NavigationMenuList>
+                  <NavigationMenuItem>
+                    <NavigationMenuTrigger className="cursor-pointer">
+                      Mega Menu
+                    </NavigationMenuTrigger>
+                    <NavigationMenuContent>
+                      <div className="flex items-center gap-2 text-md p-2 cursor-pointer font-bold hover:bg-[var(--input)]  duration-300 rounded-sm h-12">
+                        <NavigationMenuLink className="hover:bg-transparent">
+                          Link
+                        </NavigationMenuLink>
+                      </div>
+                    </NavigationMenuContent>
+                  </NavigationMenuItem>
+                </NavigationMenuList>
+              </NavigationMenu>
+            </li>
+            <li>
+              <NavigationMenu>
+                <NavigationMenuList>
+                  <NavigationMenuItem>
+                    <NavigationMenuTrigger className="cursor-pointer">
+                      Full Screen Menu
+                    </NavigationMenuTrigger>
+                    <NavigationMenuContent>
+                      <div className="flex items-center gap-2 text-md p-2 cursor-pointer font-bold hover:bg-[var(--input)]  duration-300 rounded-sm h-12">
+                        <NavigationMenuLink className="hover:bg-transparent">
+                          Link
+                        </NavigationMenuLink>
+                      </div>
+                    </NavigationMenuContent>
+                  </NavigationMenuItem>
+                </NavigationMenuList>
+              </NavigationMenu>
+            </li>
+            <li>
+              <NavigationMenu>
+                <NavigationMenuList>
+                  <NavigationMenuItem>
+                    <NavigationMenuTrigger className="cursor-pointer">
+                      Pages
+                    </NavigationMenuTrigger>
+                    <NavigationMenuContent>
+                      <div className="flex items-center gap-2 text-md p-2 cursor-pointer font-bold hover:bg-[var(--input)]  duration-300 rounded-sm h-12">
+                        <NavigationMenuLink className="hover:bg-transparent">
+                          Link
+                        </NavigationMenuLink>
+                      </div>
+                    </NavigationMenuContent>
+                  </NavigationMenuItem>
+                </NavigationMenuList>
+              </NavigationMenu>
+            </li>
+            <li>
+              <NavigationMenu>
+                <NavigationMenuList>
+                  <NavigationMenuItem>
+                    <NavigationMenuTrigger className="cursor-pointer">
+                      User Account
+                    </NavigationMenuTrigger>
+                    <NavigationMenuContent>
+                      <div className="flex items-center gap-2 text-md p-2 cursor-pointer font-bold hover:bg-[var(--input)]  duration-300 rounded-sm h-12">
+                        <NavigationMenuLink className="hover:bg-transparent">
+                          Market
+                        </NavigationMenuLink>
+                      </div>
+                      <div className="flex items-center gap-2 text-md p-2 cursor-pointer font-bold hover:bg-[var(--input)]  duration-300 rounded-sm h-12">
+                        <NavigationMenuLink className="hover:bg-transparent">
+                          Gadget
+                        </NavigationMenuLink>
+                      </div>
+                      <div className="flex items-center gap-2 text-md p-2 cursor-pointer font-bold hover:bg-[var(--input)]  duration-300 rounded-sm h-12">
+                        <NavigationMenuLink className="hover:bg-transparent">
+                          Fashion
+                        </NavigationMenuLink>
+                      </div>
+                      <div className="flex items-center gap-2 text-md p-2 cursor-pointer font-bold hover:bg-[var(--input)]  duration-300 rounded-sm h-12">
+                        <NavigationMenuLink className="hover:bg-transparent">
+                          Electronics
+                        </NavigationMenuLink>
+                      </div>
+                    </NavigationMenuContent>
+                  </NavigationMenuItem>
+                </NavigationMenuList>
+              </NavigationMenu>
+            </li>
+            <li>
+              <NavigationMenu>
+                <NavigationMenuList>
+                  <NavigationMenuItem>
+                    <NavigationMenuTrigger className="cursor-pointer">
+                      Vendor Account
+                    </NavigationMenuTrigger>
+                    <NavigationMenuContent>
+                      <div className="flex items-center gap-2 text-md p-2 cursor-pointer font-bold hover:bg-[var(--input)]  duration-300 rounded-sm h-12">
+                        <NavigationMenuLink className="hover:bg-transparent">
+                          Link
+                        </NavigationMenuLink>
+                      </div>
+                    </NavigationMenuContent>
+                  </NavigationMenuItem>
+                </NavigationMenuList>
+              </NavigationMenu>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default NavigationHeader;
