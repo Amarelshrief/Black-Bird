059326d0b7aa00b55962d1e339a3e92a9908df35
Amarelshrief <amarelshrief511@gmail.com>
2025-04-04 22:34:00 +0200

    Create "Header.tsx" component and make it a RootLayout

---
+++ src/components/Header/Headers.tsx
@@ -0,0 +1,15 @@
+import MainHeader from "./MainHeader";
+import NavigationHeader from "./NavigationHeader";
+import SecondHeader from "./SecondHeader";
+
+function Headers() {
+  return (
+    <header>
+      <MainHeader />
+      <SecondHeader />
+      <NavigationHeader />
+    </header>
+  );
+}
+
+export default Headers;
