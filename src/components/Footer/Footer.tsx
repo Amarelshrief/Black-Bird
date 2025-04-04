12ab1252b89d3b12e929a30b473faff76dbf3bf9
Amarelshrief <amarelshrief511@gmail.com>
2025-04-04 22:34:31 +0200

    Some changes of styles on "Footer.tsx" component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,100 +5,110 @@ import { Link } from "react-router-dom";
 function Footer() {
   return (
     <footer>
-      <div className="bg-[var(--primary)] pb-8">
+      <div className="bg-[var(--foreground)] pb-8">
         <div className="main-container grid grid-cols-4 gap-18 p-2">
           <div>
-            <div className="flex items-center gap-1 cursor-pointer pb-2">
+            <div className="flex items-center gap-1 pb-2 text-[var(--primary)]">
               <Bird />
               <h1 className="text-2xl font-bold">BlackBird</h1>
             </div>
-            <p className="text-sm leading-6">
+            <p className="text-sm leading-6 text-[var(--secondary)]">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Auctor
               libero id et, in gravida. Sit diam duis mauris nulla cursus. Erat
               et lectus vel ut sollicitudin elit at amet.
             </p>
-            <div className="cursor-pointer flex items-center gap-4 mt-3">
+            <div className="flex items-center gap-4 mt-3">
               <Button
                 variant="ghost"
-                className="bg-[var(--secondary)] hover:bg-[var(--secondary)]/40 hover:text-[#bb6b59] duration-150"
+                className="bg-[var(--secondary)] hover:bg-[var(--muted)] hover:text-[var(--primary)] duration-150 cursor-pointer"
               >
                 <Link to={"/"}> Shop Now </Link>
               </Button>
               <Button
                 variant="ghost"
-                className="bg-[var(--secondary)] hover:bg-[var(--secondary)]/40 hover:text-[#bb6b59] duration-150"
+                className="bg-[var(--secondary)] hover:bg-[var(--muted)] hover:text-[var(--primary)] duration-150 cursor-pointer"
               >
                 <Link to={"/"}> Send Message </Link>
               </Button>
             </div>
           </div>
           <div className="flex flex-col gap-4">
-            <h2 className="text-lg font-bold">About Us</h2>
+            <h2 className="text-lg text-[var(--primary)] font-bold">
+              About Us
+            </h2>
             <ul className="flex flex-col gap-2 w-3/4">
               <Link to={"/"}>
-                <li className="cursor-pointer hover:text-[#bb6b59] text-sm duration-150">
+                <li className="cursor-pointer text-[var(--secondary)] hover:text-[var(--accent)] text-sm duration-150">
                   Careers
                 </li>
               </Link>
               <Link to={"/"}>
-                <li className="cursor-pointer hover:text-[#bb6b59] text-sm duration-150">
+                <li className="cursor-pointer text-[var(--secondary)] hover:text-[var(--accent)] text-sm duration-150">
                   Our Stores
                 </li>
               </Link>
               <Link to={"/"}>
-                <li className="cursor-pointer hover:text-[#bb6b59] text-sm duration-150">
+                <li className="cursor-pointer text-[var(--secondary)] hover:text-[var(--accent)] text-sm duration-150">
                   Our Cares
                 </li>
               </Link>
               <Link to={"/"}>
-                <li className="cursor-pointer hover:text-[#bb6b59] text-sm duration-150">
+                <li className="cursor-pointer text-[var(--secondary)] hover:text-[var(--accent)] text-sm duration-150">
                   Terms & Conditions
                 </li>
               </Link>
               <Link to={"/"}>
-                <li className="cursor-pointer hover:text-[#bb6b59] text-sm duration-150">
+                <li className="cursor-pointer text-[var(--secondary)] hover:text-[var(--accent)] text-sm duration-150">
                   Privacy Policy
                 </li>
               </Link>
             </ul>
           </div>
           <div className="flex flex-col gap-4">
-            <h2 className="text-lg font-bold">Customer Care</h2>
+            <h2 className="text-lg font-bold text-[var(--primary)]">
+              Customer Care
+            </h2>
             <ul className="flex flex-col gap-2 w-3/4">
               <Link to={"/"}>
-                <li className="cursor-pointer hover:text-[#bb6b59] text-sm duration-150">
+                <li className="cursor-pointer text-[var(--secondary)] hover:text-[var(--accent)] text-sm duration-150">
                   Help Center
                 </li>
               </Link>
               <Link to={"/"}>
-                <li className="cursor-pointer hover:text-[#bb6b59] text-sm duration-150">
+                <li className="cursor-pointer text-[var(--secondary)] hover:text-[var(--accent)] text-sm duration-150">
                   Track Orders
                 </li>
               </Link>
               <Link to={"/"}>
-                <li className="cursor-pointer hover:text-[#bb6b59] text-sm duration-150">
+                <li className="cursor-pointer text-[var(--secondary)] hover:text-[var(--accent)] text-sm duration-150">
                   Corporate & Bulk Purchasing
                 </li>
               </Link>
               <Link to={"/"}>
-                <li className="cursor-pointer hover:text-[#bb6b59] text-sm duration-150">
+                <li className="cursor-pointer text-[var(--secondary)] hover:text-[var(--accent)] text-sm duration-150">
                   Returns & Refunds
                 </li>
               </Link>
             </ul>
           </div>
           <div className="flex flex-col gap-4">
-            <h2 className="text-lg font-bold">Contact Us</h2>
-            <p className="text-sm">
+            <h2 className="text-lg font-bold text-[var(--primary)]">
+              Contact Us
+            </h2>
+            <p className="text-sm text-[var(--secondary)]">
               70 Washington Square South, New York, NY 10012, United States
             </p>
-            <p className="text-sm">Email: uilib.help@gmail.com</p>
-            <p className="text-sm">Phone: +1 (212) 555-1234</p>
+            <p className="text-sm text-[var(--secondary)]">
+              Email: uilib.help@gmail.com
+            </p>
+            <p className="text-sm text-[var(--secondary)]">
+              Phone: +1 (212) 555-1234
+            </p>
             <div className="flex items-center gap-4">
-              <X className="cursor-pointer hover:text-[#bb6b59] duration-150" />
-              <Instagram className="cursor-pointer hover:text-[#bb6b59] duration-150" />
-              <Facebook className="cursor-pointer hover:text-[#bb6b59] duration-150" />
-              <Youtube className="cursor-pointer hover:text-[#bb6b59] duration-150" />
+              <X className="cursor-pointer text-[var(--secondary)] hover:text-[var(--accent)] duration-150" />
+              <Instagram className="cursor-pointer text-[var(--secondary)] hover:text-[var(--accent)] duration-150" />
+              <Facebook className="cursor-pointer text-[var(--secondary)] hover:text-[var(--accent)] duration-150" />
+              <Youtube className="cursor-pointer text-[var(--secondary)] hover:text-[var(--accent)] duration-150" />
             </div>
           </div>
         </div>
