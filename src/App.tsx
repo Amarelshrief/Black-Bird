f120119a6bcbecfc7e29b90fb1a8006de215a39c
Amarelshrief <amarelshrief511@gmail.com>
2025-04-04 23:45:45 +0200

    Add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import RootLayout from "./Pages/RootLayout";
 import Home from "./Pages/Home";
+import Cart from "./Pages/Cart";
+import Products from "./Pages/Products";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
-    children: [{ path: "/", element: <Home /> }],
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/cart", element: <Cart /> },
+      { path: "/products", element: <Products /> },
+    ],
   },
 ]);
 
