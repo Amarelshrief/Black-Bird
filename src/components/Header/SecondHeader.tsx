02bba3f457a985acaf5bd0679e7a1e568e4abff2
Amarelshrief <amarelshrief511@gmail.com>
2025-04-04 22:33:06 +0200

    Edit "SecondHeader.tsx" component

--- src/components/Header/SecondHeader.tsx
+++ src/components/Header/SecondHeader.tsx
@@ -1,14 +1,14 @@
-import { ShoppingCart, User } from "lucide-react";
+import { Bird, ShoppingCart, User } from "lucide-react";
 import { Input } from "../ui/input";
 
 function SecondHeader() {
   return (
     <header>
       <div className="main-container flex items-center justify-between p-2">
-        {/* <div className="flex flex-col items-center gap-1 cursor-pointer hover:text-[#bb6b59] duration-150">
-          <ShoppingBag />
-          <p>E-Commerce</p>
-        </div> */}
+        <div className="flex items-center justify-between gap-1 cursor-pointer text-[var(--primary)]">
+          <Bird />
+          <h1 className="text-2xl font-bold">BlackBird</h1>
+        </div>
         <div className="w-2/3">
           <Input
             type="search"
@@ -17,8 +17,8 @@ function SecondHeader() {
           />
         </div>
         <div className="flex items-center gap-4">
-          <ShoppingCart className="cursor-pointer hover:text-[#bb6b59] duration-150" />
-          <User className="cursor-pointer hover:text-[#bb6b59] duration-150" />
+          <User className="cursor-pointer text-[var(--primary)] hover:text-[var(--secondary)] duration-150" />
+          <ShoppingCart className="cursor-pointer text-[var(--primary)] hover:text-[var(--secondary)] duration-150" />
         </div>
       </div>
     </header>
