c61067ceb9880d374b447593fff3943cad13365b
Amarelshrief <amarelshrief511@gmail.com>
2025-04-05 16:41:16 +0200

    Add "Advantages.tsx" component and "OurProducts.tsx" component to "Home.tsx" page

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,9 +1,13 @@
+import Advantages from "@/components/Advantages/Advantages";
 import Hero from "@/components/Hero/Hero";
+import OurProducts from "@/components/OurProducts/OurProducts";
 
 function Home() {
   return (
     <>
       <Hero />
+      <Advantages />
+      <OurProducts />
     </>
   );
 }
