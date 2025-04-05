4aca9cd0cf1b436c63227964996453a892775220
Amarelshrief <amarelshrief511@gmail.com>
2025-04-05 04:09:01 +0200

    Create "index.ts" file to use images

---
+++ src/assets/index.ts
@@ -0,0 +1,6 @@
+import bannerImg1 from "./banner-15.jpg";
+import bannerImg2 from "./banner-16.jpg";
+import bannerImg3 from "./banner-17.jpg";
+import bannerImg4 from "./banner-25.jpg";
+
+export { bannerImg1, bannerImg2, bannerImg3, bannerImg4 };
