0e5141c7456cb96d52b720e1a7f190a25f82f6c2
Amarelshrief <amarelshrief511@gmail.com>
2025-04-07 22:27:44 +0200

    Add the new components to the "Home.tsx" page

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,8 +1,14 @@
 import Advantages from "@/components/Advantages/Advantages";
 import Announce from "@/components/Annoucne/Announce";
+import ElecProducts from "@/components/ElecProducts/ElecProducts";
+import FeaturedBrands from "@/components/FeaturedBrands/FeaturedBrands";
 import Hero from "@/components/Hero/Hero";
+import LongBanner from "@/components/LongBanner/LongBanner";
+import Market from "@/components/Market/Market";
+import MenFashion from "@/components/MenFashion/MenFashion";
 import OurProducts from "@/components/OurProducts/OurProducts";
 import ProductsList from "@/components/ProductsList/ProductsList";
+import WomenFashion from "@/components/WomenFashion/WomenFashion";
 
 function Home() {
   return (
@@ -12,6 +18,12 @@ function Home() {
       <OurProducts />
       <ProductsList />
       <Announce />
+      <ElecProducts />
+      <Market />
+      <MenFashion />
+      <LongBanner />
+      <WomenFashion />
+      <FeaturedBrands />
     </>
   );
 }
