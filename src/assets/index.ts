b8313984c5a11b09a8de29151ee6adea6f0f8c60
Amarelshrief <amarelshrief511@gmail.com>
2025-04-07 22:20:33 +0200

    create variable of images

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,6 +2,9 @@ import bannerImg1 from "./banner-15.jpg";
 import bannerImg2 from "./banner-16.jpg";
 import bannerImg3 from "./banner-17.jpg";
 import bannerImg4 from "./banner-25.jpg";
+import bannerImg5 from "./banner-21.jpg";
+import bannerImg6 from "./banner-22.jpg";
+import longBannerImg from "./long-banner.jpg";
 import camera from "./camera.png";
 import gaming from "./gaming.png";
 import furniture from "./furniture.png";
@@ -11,12 +14,20 @@ import toys from "./toys.png";
 import announce1 from "./annouce1.webp";
 import announce2 from "./annouce2.webp";
 import announce3 from "./annouce3.webp";
+import alibaba from "./alibaba.webp";
+import levis from "./levis.webp";
+import lotto from "./lotto.webp";
+import raymond from "./raymond.webp";
+import samsung from "./samsung.webp";
 
 export {
   bannerImg1,
   bannerImg2,
   bannerImg3,
   bannerImg4,
+  bannerImg5,
+  bannerImg6,
+  longBannerImg,
   camera,
   gaming,
   furniture,
@@ -26,4 +37,9 @@ export {
   announce1,
   announce2,
   announce3,
+  alibaba,
+  levis,
+  lotto,
+  raymond,
+  samsung,
 };
