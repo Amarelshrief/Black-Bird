fdaf775b7688610e43b083200a8dd4263784bc8b
Amarelshrief <amarelshrief511@gmail.com>
2025-04-07 23:48:25 +0200

    Add dark mode to the "SecondHeader.tsx" component

--- src/components/Header/MainHeader.tsx
+++ src/components/Header/MainHeader.tsx
@@ -1,21 +1,7 @@
-import { useState } from "react";
-import { Facebook, Instagram, Sun, Moon, X } from "lucide-react";
+import { Facebook, Instagram, X } from "lucide-react";
 import { Link } from "react-router-dom";
-import {
-  DropdownMenu,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { useTheme } from "@/components/Theme/Theme";
 
 function MainHeader() {
-  const [dark, setDark] = useState(
-    localStorage.getItem("black-bird-theme") === "dark"
-  );
-  const { setTheme } = useTheme();
-
-  function toggleDark() {
-    setDark(!dark);
-  }
   return (
     <header className="bg-[var(--foreground)] pb-2">
       <div className="main-container flex items-center justify-between p-2">
@@ -24,17 +10,6 @@ function MainHeader() {
         </h2>
         <div>
           <ul className="flex items-center gap-6 text-[var(--primary)]">
-            <li className="text-2xl cursor-pointer hover:text-[var(--secondary)] flex items-center justify-center duration-150">
-              <DropdownMenu>
-                <DropdownMenuTrigger
-                  className="cursor-pointer"
-                  onClick={toggleDark}
-                >
-                  {dark && <Sun onClick={() => setTheme("light")} />}
-                  {!dark && <Moon onClick={() => setTheme("dark")} />}
-                </DropdownMenuTrigger>
-              </DropdownMenu>
-            </li>
             {/* <li>EN</li> */}
             <Link to={"/"}>
               <li className="text-2xl cursor-pointer hover:text-[var(--secondary)] flex items-center justify-center duration-150">
