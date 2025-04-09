189fd5ab555ce31a37586ce4a20f80417216cf28
Amarelshrief <amarelshrief511@gmail.com>
2025-04-10 00:56:49 +0200

    Handle responsive design of "LongBanner.tsx" component and add background image to it

--- src/components/LongBanner/LongBanner.tsx
+++ src/components/LongBanner/LongBanner.tsx
@@ -1,30 +1,24 @@
-import { longBannerImg } from "@/assets";
 import { Button } from "../ui/button";
 
 function LongBanner() {
   return (
     <section className="main-container m-8">
-      <div className="relative flex items-center">
-        <div className="w-full">
-          <img
-            src={longBannerImg}
-            alt="ski-clothes"
-            className="w-full object-cover rounded-xl"
-          />
-        </div>
-        <div className="absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center gap-2 p-12">
-          <p className="tracking-widest text-4xl font-bold">
-            GIFT <span className="text-[var(--primary)]">50% OFF</span> PERFECT
-            STYLES
-          </p>
-          <p className="">
-            Only until the end of this week. Terms and conditions apply
-          </p>
-        </div>
-        <div className="absolute top-0 left-0 w-full h-full flex flex-col items-end justify-center gap-4 p-8">
-          <Button className="h-12 w-[9rem] text-lg text-[var(--input)] bg-[var(--primary)] hover:bg-[var(--primary)]/80 duration-300 cursor-pointer">
-            Discover Now
-          </Button>
+      <div className="bg">
+        <div className="flex flex-col items-center justify-center">
+          <div className="w-full h-full flex flex-col items-center justify-center gap-2 p-4">
+            <p className="tracking-widest text-4xl font-bold">
+              GIFT <span className="text-[var(--primary)]">50% OFF</span>{" "}
+              PERFECT STYLES
+            </p>
+            <p className="">
+              Only until the end of this week. Terms and conditions apply
+            </p>
+            <div className="">
+              <Button className="h-12 w-[9rem] text-lg text-[var(--input)] bg-[var(--primary)] hover:bg-[var(--primary)]/80 duration-300 cursor-pointer">
+                Discover Now
+              </Button>
+            </div>
+          </div>
         </div>
       </div>
     </section>
