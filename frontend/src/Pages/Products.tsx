29bdf60ee88b39e4459b21eec5f21b377e8eb399
Amarelshrief <amarelshrief511@gmail.com>
2025-04-10 23:47:21 +0200

    Add "ProductsComp.tsx" component to "Products.tsx" page and create and interfaces

--- frontend/src/Pages/Products.tsx
+++ frontend/src/Pages/Products.tsx
@@ -1,5 +1,27 @@
+import ProductsComp from "@/components/Products/ProductsComp";
+
+export interface Product {
+  id: string;
+  title: string;
+  price: number;
+  description: string;
+  rate: number;
+  category: string;
+  image: {
+    url: string;
+  };
+}
+
+export interface Products {
+  data: Product[];
+}
+
 function Products() {
-  return <div>Products</div>;
+  return (
+    <>
+      <ProductsComp />
+    </>
+  );
 }
 
 export default Products;
