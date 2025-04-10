05f5d32bbfbd18d8a9c53957c54e4a823f2be1a0
Amarelshrief <amarelshrief511@gmail.com>
2025-04-10 23:58:19 +0200

    Add responsive design for "ProductComp.tsx" component

--- frontend/src/components/Products/ProductsComp.tsx
+++ frontend/src/components/Products/ProductsComp.tsx
@@ -65,7 +65,9 @@ function ProductsComp() {
           </div>
         </div>
       </div>
-      <div className="grid grid-cols-4 gap-8 mt-8">{content}</div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 mt-8">
+        {content}
+      </div>
     </section>
   );
 }
