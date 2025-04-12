6468299474254638360d1d022be5eef3db56f417
Amarelshrief <amarelshrief511@gmail.com>
2025-04-12 23:13:30 +0200

    remove unnecessary lines

--- frontend/src/API/Api.ts
+++ frontend/src/API/Api.ts
@@ -2,7 +2,6 @@ import axios from "axios";
 
 export async function getProducts() {
   const data = await axios.get("http://localhost:1337/api/products?populate=*");
-  console.log(data.data.data);
 
   //   await new Promise((resolve) => setTimeout(resolve, 1000));
 
