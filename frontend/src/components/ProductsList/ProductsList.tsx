163ec38d26f0a6523c8e96e087f98b4fc355ff9f
Amarelshrief <amarelshrief511@gmail.com>
2025-04-13 23:37:04 +0200

    Nothing mentioned

--- frontend/src/components/ProductsList/ProductsList.tsx
+++ frontend/src/components/ProductsList/ProductsList.tsx
@@ -41,52 +41,46 @@ function ProductsList() {
     );
   }
 
-  for (let i = 0; i < 8; i++) {
-    if (data) {
-      console.log(data.data[i]);
-
-      content = data.data.map((product: Product) => (
-        <CarouselItem
-          key={product.id}
-          className="flex items-center border p-8 rounded-2xl sm:basis-1/3 lg:basis-1/5 hover:border hover:border-[var(--primary)] duration-300"
-        >
-          <div className="flex flex-col gap-2 rounded-xl h-full">
-            <div className="pb-4 flex gap-2 items-center">
-              <img
-                src={`${import.meta.env.VITE_BASE_URL}${product.image.url}`}
-                alt={product.title}
-                className="hover:scale-110 duration-300 cursor-pointer rounded-2xl"
-              />
-            </div>
-            <div className="flex flex-col items-center gap-2">
-              <p className="text-[var(--secondary)] font-bold">
-                {product.title}
-              </p>
-              <p className="text-[var(--secondary)] font-bold">
-                ${product.price}
-              </p>
-              <p className="flex items-center justify-center">
-                <Rating
-                  initialValue={product.rate}
-                  className="relative bottom-[2px] w-full"
-                  iconsCount={5}
-                  readonly={true}
-                  SVGclassName={`inline-block`}
-                  allowFraction={true}
-                  size={22}
-                />
-              </p>
-              <Button
-                variant="outline"
-                className="w-full text-[var(--secondary)] hover:bg-[var(--primary)]/80 duration-200 cursor-pointer"
-              >
-                Add to Cart
-              </Button>
-            </div>
+  if (data) {
+    content = data.data.map((product: Product) => (
+      <CarouselItem
+        key={product.id}
+        className="flex items-center border p-8 rounded-2xl sm:basis-1/3 lg:basis-1/5 hover:border hover:border-[var(--primary)] duration-300"
+      >
+        <div className="flex flex-col gap-2 rounded-xl h-full">
+          <div className="pb-4 flex gap-2 items-center">
+            <img
+              src={`${import.meta.env.VITE_BASE_URL}${product.image.url}`}
+              alt={product.title}
+              className="hover:scale-110 duration-300 cursor-pointer rounded-2xl"
+            />
           </div>
-        </CarouselItem>
-      ));
-    }
+          <div className="flex flex-col items-center gap-2">
+            <p className="text-[var(--secondary)] font-bold">{product.title}</p>
+            <p className="text-[var(--secondary)] font-bold">
+              ${product.price}
+            </p>
+            <p className="flex items-center justify-center">
+              <Rating
+                initialValue={product.rate}
+                className="relative bottom-[2px] w-full"
+                iconsCount={5}
+                readonly={true}
+                SVGclassName={`inline-block`}
+                allowFraction={true}
+                size={22}
+              />
+            </p>
+            <Button
+              variant="outline"
+              className="w-full text-[var(--secondary)] hover:bg-[var(--primary)]/80 duration-200 cursor-pointer"
+            >
+              Add to Cart
+            </Button>
+          </div>
+        </div>
+      </CarouselItem>
+    ));
   }
 
   return (
