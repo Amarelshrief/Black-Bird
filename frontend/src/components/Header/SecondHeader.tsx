ee8e5a245396bf177ebaa1411b05cf989ba6026d
Amarelshrief <amarelshrief511@gmail.com>
2025-04-13 23:34:57 +0200

    Handle the drawer cart and add dispatch and alot of things

--- frontend/src/components/Header/SecondHeader.tsx
+++ frontend/src/components/Header/SecondHeader.tsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useTheme } from "@/components/Theme/Theme";
 
-import { Bird, ShoppingCart, User, Sun, Moon, Minus, Plus } from "lucide-react";
+import { Bird, ShoppingCart, User, Sun, Moon, OctagonX } from "lucide-react";
 import { Input } from "../ui/input";
 import {
   DropdownMenu,
@@ -19,11 +19,22 @@ import {
   DrawerTrigger,
 } from "@/components/ui/drawer";
 import { Link } from "react-router-dom";
-import { gaming } from "@/assets";
+import { useAppSelector } from "@/hooks/useAppSelector";
+import { RootState } from "@/store/store";
+import { useAppDispatch } from "@/hooks/useAppDispatch";
+import {
+  decrementQuantity,
+  incrementQuantity,
+  removeFromCart,
+  resetCart,
+} from "@/store/productSlice";
 
 function SecondHeader() {
-  const [numOfItems, setNumOfItems] = useState(0);
-  const [quantity, setQuantity] = useState(0);
+  const [totalAmt, setTotalAmt] = useState<number>(0);
+  const productData = useAppSelector(
+    (state: RootState) => state.product.products
+  );
+  const dispatch = useAppDispatch();
   const [navBar, setNavBar] = useState<boolean>(false);
   const [dark, setDark] = useState(
     localStorage.getItem("black-bird-theme") === "dark"
@@ -38,13 +49,15 @@ function SecondHeader() {
     setNavBar(!navBar);
   }
 
-  function plusOne() {
-    setQuantity(quantity + 1);
-  }
+  useEffect(() => {
+    let price: number = 0;
+    productData.map((product) => {
+      price += product.quantity * product.price;
+      return price;
+    });
+    setTotalAmt(price);
+  }, [productData]);
 
-  function minusOne() {
-    setQuantity(quantity - 1);
-  }
   return (
     <header>
       <div className="main-container flex items-center justify-between p-2">
@@ -130,57 +143,109 @@ function SecondHeader() {
                 <DrawerTrigger>
                   <li className="flex items-center">
                     <ShoppingCart className="cursor-pointer text-[var(--primary)] hover:text-[var(--secondary)] duration-150" />
-                    {/* <span className="bg-red-500 rounded-full h-6 w-6 text-[var(--secondary)] font-bold">
-                      {numOfItems}
-                    </span> */}
+                    <span className="bg-[var(--primary)] rounded-full h-6 w-6 text-[var(--input)] font-bold">
+                      {productData.length}
+                    </span>
                   </li>
                 </DrawerTrigger>
-                <DrawerContent className="">
+                <DrawerContent className="overflow-y-scroll overflow-x-hidden">
                   <DrawerHeader>
-                    <DrawerTitle className="flex items-center gap-2">
-                      <ShoppingCart className="cursor-pointer text-[var(--primary)] hover:text-[var(--secondary)] duration-150" />
-                      <span className="text-[var(--secondary)] font-bold">
-                        {numOfItems} items
-                      </span>
+                    <DrawerTitle className="flex items-center justify-between gap-2">
+                      <div className="flex items-center gap-2">
+                        <ShoppingCart className="cursor-pointer text-[var(--primary)] hover:text-[var(--secondary)] duration-150" />
+                        <span className="text-[var(--secondary)] font-bold">
+                          {productData.length} items
+                        </span>
+                      </div>
+                      <div>
+                        <OctagonX
+                          onClick={() => dispatch(resetCart())}
+                          className="cursor-pointer text-[var(--secondary)] hover:text-[var(--primary)] duration-150"
+                        />
+                      </div>
                     </DrawerTitle>
                   </DrawerHeader>
-                  <div className="flex items-center gap-2 p-4">
-                    <div>
-                      <img src={gaming} alt="gaming" className="w-18" />
-                    </div>
-                    <div className="flex flex-col gap-2">
-                      <p className="font-bold">PRODUCT TITLE</p>
-                      <p className="text-sm text-[var(--secondary)]">
-                        PRODUCT PRICE
-                      </p>
-                      <div className="flex items-center gap-2">
-                        <Button
-                          onClick={plusOne}
-                          variant="outline"
-                          className="cursor-pointer text-[var(--secondary)] hover:bg-[var(--secondary)]/20 duration-200"
-                        >
-                          +
-                        </Button>
-                        <span>{quantity}</span>
-                        <Button
-                          onClick={minusOne}
-                          variant="outline"
-                          className="cursor-pointer text-[var(--secondary)] hover:bg-[var(--secondary)]/20 duration-200"
-                        >
-                          -
-                        </Button>
+                  {productData.map((product) => (
+                    <div className="flex items-center justify-between gap-3 p-4">
+                      <div>
+                        <img
+                          src={`${import.meta.env.VITE_BASE_URL}${
+                            product.image.url
+                          }`}
+                          alt={product.title}
+                          className="w-26 h-28"
+                        />
+                      </div>
+                      <div className="flex flex-col gap-3">
+                        <p className="font-bold text-lg text-[var(--primary)] tracking-wide">
+                          {product.title}
+                        </p>
+
+                        <p className="text-sm text-[var(--secondary)]">
+                          <span>{product.quantity}</span> *{" "}
+                          <span>{product.price}</span> = ${" "}
+                          <span>{product.quantity * product.price}</span>
+                        </p>
+                        <div className="flex items-center gap-2">
+                          <Button
+                            onClick={() =>
+                              dispatch(
+                                incrementQuantity({
+                                  id: product.id,
+                                  quantity: product.quantity,
+                                  title: product.title,
+                                  price: product.price,
+                                  description: product.description,
+                                  rate: product.rate,
+                                  category: product.category,
+                                  image: product.image,
+                                })
+                              )
+                            }
+                            variant="outline"
+                            className="cursor-pointer text-[var(--secondary)] hover:bg-[var(--secondary)]/20 duration-200"
+                          >
+                            +
+                          </Button>
+                          <span>{product.quantity}</span>
+                          <Button
+                            onClick={() =>
+                              dispatch(
+                                decrementQuantity({
+                                  id: product.id,
+                                  quantity: product.quantity,
+                                  title: product.title,
+                                  price: product.price,
+                                  description: product.description,
+                                  rate: product.rate,
+                                  category: product.category,
+                                  image: product.image,
+                                })
+                              )
+                            }
+                            variant="outline"
+                            className="cursor-pointer text-[var(--secondary)] hover:bg-[var(--secondary)]/20 duration-200"
+                          >
+                            -
+                          </Button>
+                        </div>
+                      </div>
+                      <div className="cursor-pointer text-[var(--secondary)] hover:text-[var(--primary)] duration-150">
+                        <OctagonX
+                          onClick={() => dispatch(removeFromCart(product.id))}
+                        />
                       </div>
                     </div>
-                  </div>
+                  ))}
                   <DrawerFooter>
-                    <Button className="h-[2.7rem] cursor-pointer">
-                      Checkout
+                    <Button className="h-[2.7rem] cursor-pointer text-md font-bold duration-200">
+                      Checkout Now ($ {totalAmt})
                     </Button>
                     <DrawerClose>
-                      <Link to={"/products"}>
+                      <Link to={"/cart"}>
                         <Button
                           variant={"outline"}
-                          className="w-full h-[2.7rem] cursor-pointer text-[var(--secondary)] hover:bg-[var(--secondary)]/20 duration-200"
+                          className="w-full h-[2.7rem] text-md cursor-pointer text-[var(--secondary)] hover:bg-[var(--secondary)]/20 duration-200"
                         >
                           View Cart
                         </Button>
