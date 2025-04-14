3331598fdb264cab59137038f68788a9768a41fd
Amarelshrief <amarelshrief511@gmail.com>
2025-04-14 23:49:29 +0200

    Handle headers

--- frontend/src/components/Header/Headers.tsx
+++ frontend/src/components/Header/Headers.tsx
@@ -4,8 +4,8 @@ import SecondHeader from "./SecondHeader";
 
 function Headers() {
   return (
-    <header>
-      <MainHeader />
+    <header className="fixed top-0 left-0 right-0 z-50 bg-background">
+      {/* <MainHeader /> */}
       <SecondHeader />
       <NavigationHeader />
     </header>
