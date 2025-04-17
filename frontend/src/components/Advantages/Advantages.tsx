16855d47331e8323ddcfd9fa02408ff26533d2c3
Amarelshrief <amarelshrief511@gmail.com>
2025-04-17 23:11:08 +0200

    Add id and handle some styling of "Advantage.tsx" component

--- frontend/src/components/Advantages/Advantages.tsx
+++ frontend/src/components/Advantages/Advantages.tsx
@@ -2,8 +2,8 @@ import { Truck, AlarmClock, HandCoins, CircleDollarSign } from "lucide-react";
 
 function Advantages() {
   return (
-    <section>
-      <div className="main-container grid-cols-1 grid md:grid-cols-2 lg:grid-cols-4 gap-4 w-full m-8">
+    <section className="mt-8" id="advantages">
+      <div className="main-container grid-cols-1 grid md:grid-cols-2 lg:grid-cols-4 gap-4 w-full">
         <div className="flex items-center justify-center gap-4 bg-[var(--input)] hover:bg-[var(--ring)] duration-300 rounded-xl p-5">
           <Truck className="text-[var(--primary)]" size={33} />
           <div className="flex flex-col">
