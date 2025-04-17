33b96d11520216a1df936fe22256f5cd725c4a06
Amarelshrief <amarelshrief511@gmail.com>
2025-04-17 23:12:16 +0200

    Add id and handle some styling of "LongBanner.tsx" component

--- frontend/src/components/LongBanner/LongBanner.tsx
+++ frontend/src/components/LongBanner/LongBanner.tsx
@@ -2,21 +2,23 @@ import { Button } from "../ui/button";
 
 function LongBanner() {
   return (
-    <section className="main-container m-8">
-      <div className="bg">
-        <div className="flex flex-col items-center justify-center">
-          <div className="w-full h-full flex flex-col items-center justify-center gap-2 p-6">
-            <p className="tracking-widest text-4xl font-bold">
-              GIFT <span className="text-[var(--primary)]">50% OFF</span>{" "}
-              PERFECT STYLES
-            </p>
-            <p className="">
-              Only until the end of this week. Terms and conditions apply
-            </p>
-            <div className="">
-              <Button className="h-12 w-[9rem] text-lg text-[var(--input)] bg-[var(--primary)] hover:bg-[var(--primary)]/80 duration-300 cursor-pointer">
-                Discover Now
-              </Button>
+    <section className="mt-8">
+      <div className="main-container">
+        <div className="bg">
+          <div className="flex flex-col items-center justify-center">
+            <div className="w-full h-full flex flex-col items-center justify-center gap-2 p-6">
+              <p className="tracking-widest text-4xl font-bold">
+                GIFT <span className="text-[var(--primary)]">50% OFF</span>{" "}
+                PERFECT STYLES
+              </p>
+              <p className="">
+                Only until the end of this week. Terms and conditions apply
+              </p>
+              <div className="">
+                <Button className="h-12 w-[9rem] text-lg text-[var(--input)] bg-[var(--primary)] hover:bg-[var(--primary)]/80 duration-300 cursor-pointer">
+                  Discover Now
+                </Button>
+              </div>
             </div>
           </div>
         </div>
