adb2a3e72763df0c684d92151ec41c8ca3d6db1c
Amarelshrief <amarelshrief511@gmail.com>
2025-04-17 23:11:51 +0200

    Add id and handle some styling of "FeaturedBrands.tsx" component

--- frontend/src/components/FeaturedBrands/FeaturedBrands.tsx
+++ frontend/src/components/FeaturedBrands/FeaturedBrands.tsx
@@ -2,33 +2,35 @@ import { alibaba, levis, lotto, raymond, samsung } from "@/assets";
 
 function FeaturedBrands() {
   return (
-    <section className="main-container m-8">
-      <div>
-        <h2 className="text-2xl font-bold mb-4">Featured Brands</h2>
-        <div className="flex items-center justify-center lg:justify-between gap-8 lg:gap-4 flex-wrap">
-          <div>
+    <section className="my-8" id="brands">
+      <div className="main-container">
+        <div>
+          <h2 className="text-2xl font-bold mb-4">Featured Brands</h2>
+          <div className="flex items-center justify-center lg:justify-between gap-8 lg:gap-4 flex-wrap">
             <div>
-              <img src={alibaba} alt="alibaba" />
+              <div>
+                <img src={alibaba} alt="alibaba" />
+              </div>
             </div>
-          </div>
-          <div>
             <div>
-              <img src={levis} alt="levis" />
+              <div>
+                <img src={levis} alt="levis" />
+              </div>
             </div>
-          </div>
-          <div>
             <div>
-              <img src={lotto} alt="lotto" className="rounded-xl" />
+              <div>
+                <img src={lotto} alt="lotto" className="rounded-xl" />
+              </div>
             </div>
-          </div>
-          <div>
             <div>
-              <img src={raymond} alt="raymond" className="rounded-xl" />
+              <div>
+                <img src={raymond} alt="raymond" className="rounded-xl" />
+              </div>
             </div>
-          </div>
-          <div>
             <div>
-              <img src={samsung} alt="samsung" />
+              <div>
+                <img src={samsung} alt="samsung" />
+              </div>
             </div>
           </div>
         </div>
