63db553040128e9c3416b6a0e3c31e6fa9a2aba0
Amarelshrief <amarelshrief511@gmail.com>
2025-04-17 23:07:25 +0200

    Add "CartItems.tsx" component

--- frontend/src/Pages/Cart.tsx
+++ frontend/src/Pages/Cart.tsx
@@ -1,70 +1,67 @@
-import { useState } from "react";
 import CartItems from "@/components/CartItems/CartItems";
-import { Button } from "@/components/ui/button";
-import Details from "@/components/CartItems/Details";
-import Payment from "@/components/CartItems/Payment";
 
-const CHECKOUT_STEPS = [
-  {
-    name: "Cart",
-    Component: () => <CartItems />,
-  },
-  {
-    name: "Details",
-    Component: () => <Details />,
-  },
-  {
-    name: "Payment",
-    Component: () => <Payment />,
-  },
-];
+// const CHECKOUT_STEPS = [
+//   {
+//     name: "Cart",
+//     Component: () => <CartItems />,
+//   },
+//   {
+//     name: "Details",
+//     Component: () => <Details />,
+//   },
+//   {
+//     name: "Payment",
+//     Component: () => <Payment />,
+//   },
+// ];
 
-export function CheckoutStepper() {
-  const [currentStep, setCurrentStep] = useState(1);
-  const [isComplete, setIsComplete] = useState(false);
+// function CheckoutStepper() {
+//   const [currentStep, setCurrentStep] = useState(1);
+//   const [isComplete, setIsComplete] = useState(false);
 
-  function calculateProgressBarWidth() {
-    return ((currentStep - 1) / (CHECKOUT_STEPS.length - 1)) * 75;
-  }
+//   function calculateProgressBarWidth() {
+//     return ((currentStep - 1) / (CHECKOUT_STEPS.length - 1)) * 75;
+//   }
 
-  const ActiveComponent = CHECKOUT_STEPS[currentStep - 1]?.Component;
-  return (
-    <div className="main-container m-24">
-      <div className="step-item">
-        {CHECKOUT_STEPS.map((Step, index) => (
-          <div className="flex items-center justify-center" key={index}>
-            <Button
-              variant={"outline"}
-              className={`w-[8rem] cursor-pointer rounded-2xl ${
-                currentStep > index || isComplete
-                  ? "text-[var(--input)] bg-[var(--primary)]"
-                  : ""
-              }`}
-              onClick={() => setCurrentStep(index + 1)}
-            >
-              {Step.name}
-            </Button>
-          </div>
-        ))}
-        <div className="">
-          <div
-            className="absolute top-1/2 left-0 h-1 bg-[var(--primary)] -z-10"
-            style={{
-              width: `${calculateProgressBarWidth()}%`,
-              transition: "ease 0.8s",
-            }}
-          />
-        </div>
-      </div>
-      <ActiveComponent />
-    </div>
-  );
-}
+//   const ActiveComponent = CHECKOUT_STEPS[currentStep - 1]?.Component;
+//   return (
+//     <div className="main-container m-24">
+//       <div className="step-item">
+//         {CHECKOUT_STEPS.map((Step, index) => (
+//           <div className="flex items-center justify-center" key={index}>
+//             <Button
+//               variant={"outline"}
+//               className={`w-[8rem] cursor-pointer rounded-2xl ${
+//                 currentStep > index || isComplete
+//                   ? "text-[var(--input)] bg-[var(--primary)]"
+//                   : ""
+//               }`}
+//               onClick={() => setCurrentStep(index + 1)}
+//             >
+//               {Step.name}
+//             </Button>
+//           </div>
+//         ))}
+
+//         <div className="">
+//           <div
+//             className="absolute top-1/2 left-0 h-1 bg-[var(--primary)] -z-10"
+//             style={{
+//               width: `${calculateProgressBarWidth()}%`,
+//               transition: "ease 0.8s",
+//             }}
+//           />
+//         </div>
+//       </div>
+//       <ActiveComponent />
+//     </div>
+//   );
+// }
 
 function Cart() {
   return (
     <>
-      <CheckoutStepper />
+      <CartItems />
     </>
   );
 }
