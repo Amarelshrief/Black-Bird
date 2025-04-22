326db16fa3397e9ff713240a1f80aa65c94b0414
Amarelshrief <amarelshrief511@gmail.com>
2025-04-22 23:28:45 +0200

    Handle images styling

--- frontend/src/components/FeaturedBrands/FeaturedBrands.tsx
+++ frontend/src/components/FeaturedBrands/FeaturedBrands.tsx
@@ -9,27 +9,47 @@ function FeaturedBrands() {
           <div className="flex items-center justify-center lg:justify-between gap-8 lg:gap-4 flex-wrap">
             <div>
               <div>
-                <img src={alibaba} alt="alibaba" />
+                <img
+                  src={alibaba}
+                  alt="alibaba"
+                  className="hover:scale-110 duration-300 cursor-pointer rounded-xl"
+                />
               </div>
             </div>
             <div>
               <div>
-                <img src={levis} alt="levis" />
+                <img
+                  src={levis}
+                  alt="levis"
+                  className="hover:scale-110 duration-300 cursor-pointer rounded-xl"
+                />
               </div>
             </div>
             <div>
               <div>
-                <img src={lotto} alt="lotto" className="rounded-xl" />
+                <img
+                  src={lotto}
+                  alt="lotto"
+                  className="hover:scale-110 duration-300 cursor-pointer rounded-xl"
+                />
               </div>
             </div>
             <div>
               <div>
-                <img src={raymond} alt="raymond" className="rounded-xl" />
+                <img
+                  src={raymond}
+                  alt="raymond"
+                  className="hover:scale-110 duration-300 cursor-pointer rounded-xl"
+                />
               </div>
             </div>
             <div>
               <div>
-                <img src={samsung} alt="samsung" />
+                <img
+                  src={samsung}
+                  alt="samsung"
+                  className="hover:scale-110 duration-300 cursor-pointer rounded-xl"
+                />
               </div>
             </div>
           </div>
