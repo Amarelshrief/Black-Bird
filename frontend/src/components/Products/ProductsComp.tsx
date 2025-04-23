7bf7b042fdadd9db19a6ee0d2864128a1d61e85a
Amarelshrief <amarelshrief511@gmail.com>
2025-04-23 23:23:39 +0200

    Handling filtering and fetch the data by tan stack query

--- frontend/src/components/Products/ProductsComp.tsx
+++ frontend/src/components/Products/ProductsComp.tsx
@@ -2,14 +2,59 @@ import { Button } from "../ui/button";
 import { Skeleton } from "@/components/ui/skeleton";
 import ProductCard from "./ProductCard";
 import { useQuery } from "@tanstack/react-query";
-import { getProducts } from "@/API/Api";
 import { Product } from "@/Pages/Products";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function ProductsComp() {
+  const url = "http://localhost:1337/api/products?populate=*";
+  const [products, setProducts] = useState(url);
+  const [active1, setActive1] = useState(true);
+  const [active2, setActive2] = useState(false);
+  const [active3, setActive3] = useState(false);
+  const [active4, setActive4] = useState(false);
   const productQuery = useQuery({
     queryKey: ["products"],
-    queryFn: getProducts,
+    async queryFn() {
+      const data = await axios.get(products);
+      return data.data;
+    },
   });
+
+  useEffect(() => {
+    productQuery.refetch();
+  }, [products]);
+
+  function handleAllProducts() {
+    setProducts("http://localhost:1337/api/products?populate=*");
+    setActive1(true);
+    setActive2(false);
+    setActive3(false);
+    setActive4(false);
+  }
+
+  function handleMenProducts() {
+    setProducts(url + "&filters[category][$eq]=men");
+    setActive2(true);
+    setActive1(false);
+    setActive3(false);
+    setActive4(false);
+  }
+  function handleWomenProducts() {
+    setProducts(url + "&filters[category][$eq]=women");
+    setActive3(true);
+    setActive1(false);
+    setActive2(false);
+    setActive4(false);
+  }
+
+  function handleElectronicsProducts() {
+    setProducts(url + "&filters[category][$eq]=electronics");
+    setActive4(true);
+    setActive1(false);
+    setActive2(false);
+    setActive3(false);
+  }
 
   let content;
 
@@ -60,18 +105,55 @@ function ProductsComp() {
         </div>
         <div className="flex flex-col md:flex-row items-center gap-4">
           <div>
-            <Button className="h-10 text-lg text-input dark:text-card-foreground bg-primary hover:bg-[var(--primary)]/80 duration-200 cursor-pointer">
+            <Button
+              variant={active1 ? "default" : "outline"}
+              onClick={handleAllProducts}
+              className={`h-10 text-lg ${
+                active1
+                  ? "text-input dark:text-card-foreground"
+                  : "hover:bg-input text-secondary hover:text-primary"
+              }  duration-200 cursor-pointer`}
+            >
               All Categories
             </Button>
           </div>
           <div>
-            <Button className="h-10 text-lg text-input dark:text-card-foreground bg-primary hover:bg-[var(--primary)]/80 duration-200 cursor-pointer">
+            <Button
+              variant={active2 ? "default" : "outline"}
+              onClick={handleMenProducts}
+              className={`h-10 text-lg ${
+                active2
+                  ? "text-input dark:text-card-foreground"
+                  : "hover:bg-input text-secondary hover:text-primary"
+              }  duration-200 cursor-pointer`}
+            >
               Men Category
             </Button>
           </div>
           <div>
-            <Button className="h-10 text-lg text-input dark:text-card-foreground bg-primary hover:bg-[var(--primary)]/80 duration-200 cursor-pointer">
+            <Button
+              variant={active3 ? "default" : "outline"}
+              onClick={handleWomenProducts}
+              className={`h-10 text-lg ${
+                active3
+                  ? "text-input dark:text-card-foreground"
+                  : "hover:bg-input text-secondary hover:text-primary"
+              }  duration-200 cursor-pointer`}
+            >
               Women Category
+            </Button>
+          </div>
+          <div>
+            <Button
+              variant={active4 ? "default" : "outline"}
+              onClick={handleElectronicsProducts}
+              className={`h-10 text-lg ${
+                active4
+                  ? "text-input dark:text-card-foreground"
+                  : "hover:bg-input text-secondary hover:text-primary"
+              }  duration-200 cursor-pointer`}
+            >
+              Electronics Category
             </Button>
           </div>
         </div>
