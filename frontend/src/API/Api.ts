a9150a15612d4f62ad2e261b5b42653821e33aa9
Amarelshrief <amarelshrief511@gmail.com>
2025-04-23 23:17:02 +0200

    Delete "Api.tsx" file

--- frontend/src/API/Api.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export async function getProducts() {
-  const data = await axios.get("http://localhost:1337/api/products?populate=*");
-
-  //   await new Promise((resolve) => setTimeout(resolve, 1000));
-
-  return data.data;
-}
