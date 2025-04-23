319257499372d5e0a0a21a68fe9e85cf90a0cee0
Amarelshrief <amarelshrief511@gmail.com>
2025-04-23 23:19:48 +0200

    Initialize images of the project

--- frontend/src/assets/index.ts
+++ frontend/src/assets/index.ts
@@ -21,6 +21,29 @@ import raymond from "./raymond.webp";
 import samsung from "./samsung.webp";
 import cartImg from "./bgCartImg.jpg";
 import menuIcon from "./store.svg";
+import preview1 from "./preview_1.jpg";
+import preview2 from "./preview_2.jpg";
+import preview3 from "./preview_3.jpg";
+import preview4 from "./preview_4.jpg";
+import preview5 from "./preview_5.png";
+import preview6 from "./preview_6.jpg";
+import preview7 from "./preview_7.jpg";
+import preview8 from "./preview_8.jpg";
+import preview9 from "./preview_9.jpg";
+import preview10 from "./preview_10.jpg";
+import preview11 from "./preview_11.jpg";
+import preview12 from "./preview_12.jpg";
+import preview13 from "./preview_13.png";
+import preview14 from "./preview_14.jpg";
+import preview15 from "./preview_15.jpg";
+import preview16 from "./preview_16.jpg";
+import elec1 from "./elec1.png";
+import elec2 from "./elec2.png";
+import elec3 from "./elec3.png";
+import elec4 from "./elec4.png";
+import elec5 from "./elec5.png";
+import elec6 from "./elec6.png";
+import elec7 from "./elec7.png";
 
 export {
   bannerImg1,
@@ -46,4 +69,27 @@ export {
   samsung,
   cartImg,
   menuIcon,
+  preview1,
+  preview2,
+  preview3,
+  preview4,
+  preview5,
+  preview6,
+  preview7,
+  preview8,
+  preview9,
+  preview10,
+  preview11,
+  preview12,
+  preview13,
+  preview14,
+  preview15,
+  preview16,
+  elec1,
+  elec2,
+  elec3,
+  elec4,
+  elec5,
+  elec6,
+  elec7,
 };
